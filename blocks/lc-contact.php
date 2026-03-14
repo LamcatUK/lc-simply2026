af292bc9e2039ed74bd4b2009737087449ec3beb
LamcatUK <git@lamcat.co.uk>
2026-03-14 18:11:12 +0000

    contact

--- blocks/lc-contact.php
+++ blocks/lc-contact.php
@@ -20,13 +20,17 @@ $extra      = $block['className'] ?? 'py-5';
 			<div class="col-md-6">
 				<h2><?= esc_html( get_field( 'title' ) ); ?></h2>
 				<div class="mb-4"><?= wp_kses_post( get_field( 'content' ) ); ?></div>
-				<div class="mb-2"><i class="bi bi-telephone-fill"></i>
-					<?= do_shortcode( '[contact_phone]' ); ?>
-				</div>
 				<div class="mb-2"><i class="bi bi-send-fill"></i>
 					<?= do_shortcode( '[contact_email]' ); ?>
 				</div>
 				<?php
+				if ( get_field( 'contact_phone', 'option' ) ) {
+					?>
+				<div class="mb-2"><i class="bi bi-telephone-fill"></i>
+					<?= do_shortcode( '[contact_phone]' ); ?>
+				</div>
+					<?php
+				}
 				$socials = get_field( 'socials', 'option' );
 				if ( $socials && ( ! empty( $socials['facebook_url'] ) || ! empty( $socials['instagram_url'] ) || ! empty( $socials['twitter_url'] ) || ! empty( $socials['pinterest_url'] ) || ! empty( $socials['youtube_url'] ) || ! empty( $socials['linkedin_url'] ) ) ) {
 					?>
